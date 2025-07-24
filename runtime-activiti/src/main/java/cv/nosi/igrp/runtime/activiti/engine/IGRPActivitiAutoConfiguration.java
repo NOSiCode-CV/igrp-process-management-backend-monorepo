fffcdcaaac742c8c91de5a41ea3c04c255f0e3ca
william.fonseca <william.fonseca@nosi.cv>
2025-07-24 16:41:02 -0100

    Add IGRPActivitiAutoConfiguration for Activiti engine configuration and package scanning

---
+++ runtime-activiti/src/main/java/cv/nosi/igrp/runtime/activiti/engine/IGRPActivitiAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cv.nosi.igrp.runtime.activiti.engine;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "cv.nosi.igrp.runtime.activiti.engine.process",
+        "cv.nosi.igrp.runtime.activiti.engine.task"
+})
+public class IGRPActivitiAutoConfiguration {
+}
