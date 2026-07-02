6632e89ad7cc8028a7e46688b68ed36fd348d939
Brocy Centeio <centeio.3face@gmail.com>
2026-07-02 16:33:37 -0100

    Remove IRN customization and integration modules

--- process-runtime-auth-irn/src/main/java/cv/igrp/framework/process/runtime/auth/irn/adapter/integration/data/Profile.java
+++
@@ -1,7 +0,0 @@
-package cv.igrp.framework.process.runtime.auth.irn.adapter.integration.data;
-
-public record Profile(
-        String profileId,
-        String profileCode,
-        String profileName
-) {}
